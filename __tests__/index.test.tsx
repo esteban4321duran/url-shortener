6db35fa691c6279755c454c93c2fa3134fe132f7
Esteban <esteban321duran@gmail.com>
2022-12-07 15:31:37 -0300

    implemented user story
    As a user I want to enter a URL to get a shortened URL
     Changes to be committed:
    	modified:   __tests__/index.test.tsx
     	modified:   pages/index.tsx

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,15 +1,59 @@
 import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
 import Home from "@/pages/index";
 
-describe("Home", () => {
+beforeEach(() => {
+    render(<Home />);
+});
+describe("rendering", () => {
     it("renders a heading", () => {
-        render(<Home />);
         const heading = screen.getByRole("heading");
         expect(heading).toBeInTheDocument();
     });
     it("renders website's name", () => {
-        render(<Home />);
         const heading = screen.getByText(/esteban's url shortener/i);
         expect(heading).toBeInTheDocument();
     });
+    it("renders output field", () => {
+        const shortenedUrlInput = screen.getByRole("textbox", {
+            name: /shortened url/i,
+        });
+        expect(shortenedUrlInput).toBeInTheDocument();
+    });
+    it("renders URLForm", () => {
+        const urlForm = selectOriginalUrlInput();
+        expect(urlForm).toBeInTheDocument();
+    });
 });
+
+describe("behaviour", () => {
+    it("sends POST request and renders shortened url from response on form submit", async () => {
+        //mock fetch
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                json: () =>
+                    Promise.resolve({
+                        originalUrl: "localhost/3Bhi",
+                    }),
+            })
+        ) as jest.Mock;
+        const originalUrlInput = selectOriginalUrlInput();
+        await user.type(originalUrlInput, "http://www.example.com");
+        const submitButton = selectSubmitButton();
+        //component calls fetch. The mocked version will be called instead
+        await user.click(submitButton);
+        //POST request is sent. The response data is rendered to the outputField
+        const outputField = screen.getByDisplayValue(/localhost\/3Bhi/i);
+        expect(outputField).toBeInTheDocument();
+    });
+});
+
+//TODO test output field is read-only
+function selectOriginalUrlInput(): HTMLInputElement {
+    return screen.getByRole("textbox", {
+        name: /original url/i,
+    });
+}
+function selectSubmitButton(): HTMLButtonElement {
+    return screen.getByRole("button", { name: /shorten/i });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,58 @@
 import Head from "next/head";
-
 import styles from "@/pages/index.module.css";
+import URLForm from "@/components/URLForm";
+import { useState, useEffect } from "react";
 const Home: React.FC = () => {
-    return <h1>Esteban's URL shortener</h1>;
+    //TODO declare types for these states
+    const [inputValues, setInputValues] = useState<any>({});
+    const [submitForm, setSubmitForm] = useState(false);
+    const [shortenedUrl, setShortenedUrl] = useState("");
+
+    function handleSubmit(inputValues: { originalUrl: string }) {
+        setInputValues((oldState: any) => {
+            return { ...oldState, ...inputValues };
+        });
+        setSubmitForm(true);
+    }
+
+    useEffect(() => {
+        if (!submitForm) return;
+        async function sendInput() {
+            const response = await fetch("/api/urls", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ originalUrl: inputValues.originalUrl }),
+            });
+            const responseData = await response.json();
+            console.log(responseData);
+            setShortenedUrl(responseData.shortenedUrl);
+            setSubmitForm(false);
+        }
+        sendInput();
+    }, [inputValues, submitForm]);
+
+    return (
+        <>
+            <Head>
+                <title>Esteban's URL shortener</title>
+            </Head>
+            <header className="flex justify-center mt-10">
+                <h1 className="font-semibold text-6xl">
+                    Esteban's URL shortener
+                </h1>
+                ;
+            </header>
+            <main className="flex flex-col space-y-8 ">
+                <URLForm submitHandler={handleSubmit} />
+                <label className="shadow-lg p-4 bg-slate-100 w-full max-w-lg  flex justify-center space-x-4">
+                    <span>Shortened URL:</span>
+                    <input type="text" value={shortenedUrl} readOnly />
+                </label>
+            </main>
+        </>
+    );
 };
 
 export default Home;

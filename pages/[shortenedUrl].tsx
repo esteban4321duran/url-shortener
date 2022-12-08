336f2adbc837fa1bd593fa5af86de4479a71cb0e
Esteban <esteban321duran@gmail.com>
2022-12-08 10:35:50 -0300

    implemented user story
    As a user I want a shortened URL to redirect me to the original URL
    
     Changes to be committed:
    	new file:   components/Spinner.tsx
    	new file:   pages/[shortenedUrl].tsx

---
+++ pages/[shortenedUrl].tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import { GetServerSidePropsContext } from "next";
+import connectionPool from "../config/db";
+import idHasher from "../config/hashIds";
+import { RowDataPacket } from "mysql2";
+import { useRouter } from "next/router";
+import Spinner from "@/components/spinner";
+import Head from "next/head";
+
+interface PropsInterface {
+    originalUrl: string | null;
+}
+
+const Redirecting: React.FC<PropsInterface> = ({ originalUrl }) => {
+    const router = useRouter();
+    const [redirect, setRedirect] = useState(false);
+    const [message, setMessage] = useState(
+        "Please wait, you are being redirected"
+    );
+    useEffect(() => {
+        if (redirect) router.replace(originalUrl!);
+    }, [redirect]);
+    useEffect(() => {
+        setTimeout(() => {
+            setRedirect(true);
+        }, 2000);
+    });
+    if (originalUrl === null)
+        setMessage("The shortened URL you provided is not valid");
+
+    return (
+        <>
+            <Head>
+                <title>Redirecting</title>
+            </Head>
+            <main className="flex justify-center items-center">
+                <div className="shadow-lg p-4 bg-slate-100 w-full max-w-lg  flex items-center space-y-4 flex-col">
+                    <p className="text-xl">{message}</p>
+                    <Spinner />
+                </div>
+            </main>
+        </>
+    );
+};
+export default Redirecting;
+
+export async function getServerSideProps(
+    context: GetServerSidePropsContext<{ shortenedUrl: string }>
+) {
+    //get originalUrl from DB
+    const shortenedUrl = context.params?.shortenedUrl!;
+
+    const [urlId] = idHasher.decode(shortenedUrl);
+    if (urlId === undefined) {
+        const props: PropsInterface = {
+            originalUrl: null,
+        };
+        return {
+            props,
+        };
+    }
+
+    const [rows, _] = await connectionPool.query(
+        "SELECT original_url AS `originalUrl` FROM urls WHERE id = ?;",
+        urlId
+    );
+
+    const [{ originalUrl }] = rows as RowDataPacket[];
+
+    const props: PropsInterface = {
+        originalUrl,
+    };
+
+    return {
+        props,
+    };
+}
